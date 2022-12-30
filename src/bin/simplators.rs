c421c54f2a239d4d87953ef3d0d6f2c8e1312feb
lighk <daoshibushi@gmail.com>
2022-12-30 14:32:55 +0800

    feat(bin): support args

--- src/bin/simplators.rs
+++ src/bin/simplators.rs
@@ -1,4 +1,4 @@
-
+use std::env;
 use log::LevelFilter;
 
 use iot_device_simulator::Dlt64507Simulator;
@@ -8,8 +8,14 @@ fn main() {
         .filter_level(LevelFilter::Debug)
         .init();
 
+    let args: Vec<String> = env::args().collect();
+    let count = match args.len() {
+        2 => *(&args[1].trim().parse::<u16>().expect("expect a u16")),
+        _ => 1
+    };
+
     let mut handles = Vec::new();
-    for i in 0..10 {
+    for i in 0..count {
         let dlt64507_simulator = Dlt64507Simulator::new(String::from((9500 + i).to_string()), 9500 + i);
         let join_handle = dlt64507_simulator.start();
         handles.push(join_handle);
